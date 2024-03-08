4b8364d04907a7ac053a7d6517b772228d317e6b
jigarsuthar12 <jigar.suthar@seaflux.tech>
2024-03-08 10:49:22 +0530

    feat: update foldername

--- recipe-api/src/module/recipe/recipe.route.ts
+++ recipe-api/src/module/recipe/recipe.route.ts
@@ -4,7 +4,7 @@ import { RecipeController } from "./recipe.controller";
 
 const router = Router();
 const recipeController = new RecipeController();
-router.get("/", Middleware.auth, recipeController.getRecipes);
-router.post("/create", Middleware.auth, recipeController.createRecipe);
-router.patch("/update/:recipeId", Middleware.auth, recipeController.updateRecipe);
+router.get("/:recipeId", Middleware.auth, recipeController.getRecipes);
+router.post("/", Middleware.auth, recipeController.createRecipe);
+router.patch("/:recipeId", Middleware.auth, recipeController.updateRecipe);
 export const recipeRoutes: Router = router;
